71d58e3e4d2b0e52294a6841fad9c7fcfacf7536
Lars Gebraad <larsgebraad@gmail.com>
2018-03-17 17:03:32 +0100

    Resolution was too high for the Linux decoder, reduced that and bitrate.

--- src/visualization/wavefield.py
+++ src/visualization/wavefield.py
@@ -6,10 +6,10 @@ import matplotlib.animation as animation
 # plt.rcParams['animation.ffmpeg_path'] = 'C:\\Program Files\\ffmpeg\\bin\\ffmpeg'
 
 ims = []
-fig = plt.figure(figsize=(16,8), dpi=300)
+fig = plt.figure(figsize=(10,5), dpi=400)
 
 Writer = animation.writers['ffmpeg']
-writer = Writer(fps=30, metadata=dict(artist='Lars Gebraad'), bitrate=8000)
+writer = Writer(fps=30, metadata=dict(artist='Lars Gebraad'), bitrate=5000)
 
 for frame in np.arange(0,3000,10):
     im = plt.imshow(np.transpose(np.loadtxt("output/vx%i.txt" % frame)), animated=True,vmin=-5e-13, vmax=5e-13, aspect=1, cmap=plt.get_cmap('seismic'))
