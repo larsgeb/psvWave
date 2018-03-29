ea9f365f1c6451cacdb48ef849fe0bbd41982aa7
Lars Gebraad <larsgebraad@gmail.com>
2018-03-29 16:50:01 +0200

    Hardcode the reduced parametrization.

--- src/forward-psv/model.cpp
+++ src/forward-psv/model.cpp
@@ -11,6 +11,24 @@ model::model() {
     arma::mat lm = la + 2 * mu;
     arma::mat b_vx = (1.0 / 1500.0) * arma::ones(nx, nz);
     arma::mat b_vz = b_vx;
+
+    paramSpans(0, 0) = col1X;
+    paramSpans(1, 0) = col1X;
+    paramSpans(2, 0) = col2X;
+    paramSpans(3, 0) = col2X;
+    paramSpans(4, 0) = col3X;
+    paramSpans(5, 0) = col3X;
+    paramSpans(6, 0) = col4X;
+    paramSpans(7, 0) = col4X;
+
+    paramSpans(0, 1) = topRowY;
+    paramSpans(1, 1) = bottomRowY;
+    paramSpans(2, 1) = topRowY;
+    paramSpans(3, 1) = bottomRowY;
+    paramSpans(4, 1) = topRowY;
+    paramSpans(5, 1) = bottomRowY;
+    paramSpans(6, 1) = topRowY;
+    paramSpans(7, 1) = bottomRowY;
 }
 
 arma::mat model::vecToField(arma::vec &vec) {
@@ -20,4 +38,21 @@ arma::mat model::vecToField(arma::vec &vec) {
 
 arma::vec model::fieldToVec(arma::mat &mat) {
     return arma::vectorise(mat);
+}
+
+void model::updateFields(arma::vec _lambda, arma::vec _mu, arma::vec _lightness) {
+
+    if (_lambda.n_elem != paramSpans.n_rows or _mu.n_elem != paramSpans.n_rows or _lightness.n_elem != paramSpans.n_rows){
+        throw std::invalid_argument("Trying to update the model with a wrong number of parameters!");
+    }
+
+    for (arma::uword iP = 0; iP < paramSpans.n_rows; ++iP) {
+        auto subnx = paramSpans(iP, 0).b - paramSpans(iP, 0).a + 1;
+        auto subnz = paramSpans(iP, 1).b - paramSpans(iP, 1).a + 1;
+        la(paramSpans(iP, 0), paramSpans(iP, 1)) = _lambda(iP) * arma::ones(subnx, subnz);
+        mu(paramSpans(iP, 0), paramSpans(iP, 1)) = (_mu(iP)) * arma::ones(subnx, subnz);
+        lm(paramSpans(iP, 0), paramSpans(iP, 1)) = (_lambda(iP) + 2 * _mu(iP)) * arma::ones(subnx, subnz);
+        b_vx(paramSpans(iP, 0), paramSpans(iP, 1)) = (_lightness(iP)) * arma::ones(subnx, subnz);
+        b_vz(paramSpans(iP, 0), paramSpans(iP, 1)) = (_lightness(iP)) * arma::ones(subnx, subnz);
+    }
 }
--- src/forward-psv/model.h
+++ src/forward-psv/model.h
@@ -18,18 +18,36 @@ public:
     const double np_factor = 0.0075;
     const arma::uword nx = nx_domain + 2 * np_boundary;
     const arma::uword nz = nz_domain + np_boundary;
-    arma::mat la = 4000000000 * arma::ones(nx, nz);
-    arma::mat mu = 1000000000 * arma::ones(nx, nz);
-    arma::mat lm = la + 2.0 * mu;
-    arma::mat b_vx = (1.0 / 1500.0) * arma::ones(nx, nz);
-    arma::mat b_vz = b_vx;
+
+    // Initialize static fields
+    arma::mat la = arma::mat(nx,nz);
+    arma::mat mu = arma::mat(nx,nz);
+    arma::mat lm = arma::mat(nx,nz);
+    arma::mat b_vx = arma::mat(nx,nz);
+    arma::mat b_vz = arma::mat(nx,nz);
+
+    // Stuff about the parametrization
+    arma::field<arma::span> paramSpans = arma::field<arma::span>(8, 2);
+    arma::span topRowY = arma::span(0, nz_domain / 2 - 1);
+    arma::span bottomRowY = arma::span(nz_domain / 2, nz - 1);
+    arma::span col1X = arma::span(0, nx_domain / 4 - 1 + np_boundary);
+    arma::span col2X = arma::span((nx_domain / 4) * 1 + np_boundary, (nx_domain / 4) * 2 + np_boundary - 1);
+    arma::span col3X = arma::span((nx_domain / 4) * 2 + np_boundary, (nx_domain / 4) * 3 + np_boundary - 1);
+    arma::span col4X = arma::span((nx_domain / 4) * 3 + np_boundary, (nx_domain / 4) * 4 + np_boundary * 2 - 1);
+
+    // Interior of domain (excluding boundary layer)
+    arma::span interiorX = arma::span(np_boundary, np_boundary + nx_domain - 1);
+    arma::span interiorZ = arma::span(0, nz_domain - 1);
 
     // Constructors
     model();
 
     // Methods
     static arma::vec fieldToVec(arma::mat &mat);
+
     static arma::mat vecToField(arma::vec &vec);
+
+    void updateFields(arma::vec lambda, arma::vec mu, arma::vec lightness);
 };
 
 
