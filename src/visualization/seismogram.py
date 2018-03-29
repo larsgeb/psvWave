3189ec9ba26294906a77805ded2030afdef94487
Lars Gebraad <larsgebraad@gmail.com>
2018-03-29 11:17:25 +0200

    Made seismogram dual component.

--- src/visualization/seismogram.py
+++ src/visualization/seismogram.py
@@ -3,10 +3,16 @@ import matplotlib
 # matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(10, 5), dpi=400)
+# fig = plt.figure(figsize=(6, 3), dpi=400)
 
-seismo = np.transpose(np.loadtxt("vxTop.txt"))
-max = np.max(np.abs(seismo[:,:]))
+seismo1 = np.transpose(np.loadtxt("output-observed/seismogram0_ux.txt"))
+seismo2 = np.transpose(np.loadtxt("output-observed/seismogram0_uz.txt"))
+max1 = np.max(np.abs(seismo1))/1000
+max2 = np.max(np.abs(seismo1))/1000
 
-im = plt.imshow(seismo[:,:], cmap=plt.get_cmap('seismic'), aspect=1 / 10, vmax=max, vmin=-max)
+
+plt.subplot(1,2,1)
+im = plt.imshow(seismo1, cmap=plt.get_cmap('seismic'), aspect=1/70, vmax=max1, vmin=-max1)
+plt.subplot(1,2,2)
+im = plt.imshow(seismo2, cmap=plt.get_cmap('seismic'), aspect=1/70, vmax=max2, vmin=-max2)
 plt.show()
