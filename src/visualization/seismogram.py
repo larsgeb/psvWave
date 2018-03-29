298146749beee735d5ced6cb764fb9b2145b6cfa
Lars Gebraad <larsgebraad@gmail.com>
2018-03-29 16:49:22 +0200

    Updated graphing tools further.

--- src/visualization/seismogram.py
+++ src/visualization/seismogram.py
@@ -5,14 +5,29 @@ import matplotlib.pyplot as plt
 
 # fig = plt.figure(figsize=(6, 3), dpi=400)
 
-seismo1 = np.transpose(np.loadtxt("output-observed/seismogram0_ux.txt"))
-seismo2 = np.transpose(np.loadtxt("output-observed/seismogram0_uz.txt"))
-max1 = np.max(np.abs(seismo1))/1000
-max2 = np.max(np.abs(seismo1))/1000
+seismoXSyn = np.transpose(np.loadtxt("seismogramSyn_ux.txt"))
+seismoXObs = np.transpose(np.loadtxt("seismogramObs_ux.txt"))
+seismoZSyn = np.transpose(np.loadtxt("seismogramSyn_uz.txt"))
+seismoZObs = np.transpose(np.loadtxt("seismogramObs_uz.txt"))
+
+seismoXDiff = seismoXObs - seismoXSyn
+seismoZDiff = seismoZObs - seismoZSyn
+
+max1 = np.max(np.abs(seismoXObs)) / 300
+max2 = np.max(np.abs(seismoZObs)) / 300
 
 
-plt.subplot(1,2,1)
-im = plt.imshow(seismo1, cmap=plt.get_cmap('seismic'), aspect=1/70, vmax=max1, vmin=-max1)
-plt.subplot(1,2,2)
-im = plt.imshow(seismo2, cmap=plt.get_cmap('seismic'), aspect=1/70, vmax=max2, vmin=-max2)
+plt.subplot(2,2,1)
+im = plt.imshow(seismoXObs, cmap=plt.get_cmap('seismic'), aspect=1 / 70, vmax=max1, vmin=-max1)
+plt.subplot(2,2,2)
+im = plt.imshow(seismoZObs, cmap=plt.get_cmap('seismic'), aspect=1 / 70, vmax=max2, vmin=-max2)
+plt.subplot(2,2,3)
+im = plt.imshow(seismoXSyn, cmap=plt.get_cmap('seismic'), aspect=1 / 70, vmax=max1, vmin=-max1)
+plt.subplot(2,2,4)
+im = plt.imshow(seismoZSyn, cmap=plt.get_cmap('seismic'), aspect=1 / 70, vmax=max2, vmin=-max2)
+
+misfit = (np.sum(np.sum(np.square(seismoXDiff))) + np.sum(np.sum(np.square(seismoZDiff))))**0.5
+
+print(misfit)
+
 plt.show()
--- src/visualization/wavefield.py
+++ src/visualization/wavefield.py
@@ -10,10 +10,11 @@ fig = plt.figure(figsize=(10,5), dpi=400)
 Writer = animation.writers['ffmpeg']
 writer = Writer(fps=30, metadata=dict(artist='Lars Gebraad'), bitrate=5000)
 
-for frame in np.arange(0,300,10):
+for frame in np.arange(0,3500,10):
     im = plt.imshow(np.transpose(np.loadtxt("output/shot0/vx%i.txt" % frame)), animated=True,vmin=-5e-13, vmax=5e-13, aspect=1, cmap=plt.get_cmap('seismic'))
     ims.append([im])
 
 
 ani = animation.ArtistAnimation(fig, ims, interval=10, blit=True, repeat_delay=0)
 ani.save('shot0.mp4', writer=writer)
+
