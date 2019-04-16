503b9714ed2ab0212781119f7573ea0d9221280e
Lars Gebraad <larsgebraad@gmail.com>
2019-04-16 21:27:51 +0200

    Automated documentation generation further.

--- docs-source/build_documentation.sh
+++ docs-source/build_documentation.sh
@@ -1,4 +1,6 @@
-conda activate forward-virieux
+#!/usr/bin/env bash
+pip install breathe
+pip install htmlmin
 cd source/doxygen
 doxygen Doxyfile
 cd ../..
@@ -7,3 +9,8 @@ rm ../docs -rf
 mkdir ../docs
 touch ../docs/.nojekyll
 mv build/html/* ../docs
+rm build -rf
+for file in ../docs/*.html
+do
+        htmlmin $file $file
+done
