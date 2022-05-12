f05485b742b98512498c9ff26bde9eadf2f09bb0
Lars Gebraad <lars.gebraad@erdw.ethz.ch>
2022-05-12 18:22:24 +0200

    Changed build system to VSCode instead of CMakeLists

--- setup.py
+++ setup.py
@@ -11,135 +11,10 @@ from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
 
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        os.system(
-            "rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info ./psvWave/*.so ./*.so "
-            "__pycache__/ .pytest_cache/ psvWave/__pycache__/ CMakeCache.txt "
-            "cmake_install.cmake CMakeFiles"
-        )
-
-
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=""):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
-class CMakeBuild(build_ext):
-    def run(self):
-
-        if not (platform.system() == "Linux" or platform.system() == "Darwin"):
-            raise RuntimeError(
-                f"Windows is not supported. Your system: {platform.system()}."
-            )
-
-        try:
-            out = subprocess.check_output(["cmake", "--version"])
-            print(out)
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: "
-                + ", ".join(e.name for e in self.extensions)
-            )
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-
-        import pybind11
-
-        # Set the right variables for PyBind. These are overwritten by env variables if
-        # defined, in CMakeLists.
-        suffix = get_config_vars()["EXT_SUFFIX"]
-        python_includes = get_paths()["include"]
-        pybind_includes = pybind11.get_include()
-
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        # required for auto-detection of auxiliary "native" libs
-        if not extdir.endswith(os.path.sep):
-            extdir += os.path.sep
-
-        cmake_args = [
-            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
-            "-DPYBIND_INCLUDES=" + pybind_includes,
-            "-DPYTHON_INCLUDES=" + python_includes,
-            "-DSUFFIX=" + suffix,
-        ]
-
-        cfg = "Debug" if self.debug else "Release"
-        build_args = ["--config", cfg]
-
-        cmake_args += [
-            "-DCMAKE_BUILD_TYPE=" + cfg,
-        ]
-        build_args += ["--", "-j2"]
-
-        env = os.environ.copy()
-        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get("CXXFLAGS", ""), self.distribution.get_version()
-        )
-
-        if platform.system() == "Darwin":
-            print("Trying to set GCC compiler manually...")
-            import glob
-
-            try:
-                gcc_loc = glob.glob("/opt/homebrew/Cellar/gcc/*/bin")[0]
-            except IndexError:
-                raise Exception("Couldn't find GCC, exiting...")
-
-            print(f"Found GCC loc at {gcc_loc}")
-
-            cmake_args += [
-                f"-DCMAKE_C_COMPILER={gcc_loc}/gcc-11",
-                f"-DCMAKE_CXX_COMPILER={gcc_loc}/g++-11",
-            ]
-
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-
-        out = subprocess.Popen(
-            ["cmake", ext.sourcedir] + cmake_args,
-            cwd=self.build_temp,
-            env=env,
-            stdout=subprocess.PIPE,
-        )
-
-        result = out.communicate()[0]
-
-        print(result.decode())
-
-        out = subprocess.Popen(
-            ["cmake", "--build", ".", "--target", "psvWave_cpp"] + build_args,
-            cwd=self.build_temp,
-        )
-
-        result = out.communicate()
-
-        print(f"CMAKE RETURN {result}")
-
-        assert out.returncode is not None
-        assert out.returncode == 0
-
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 cmd_classes = versioneer.get_cmdclass()
-cmd_classes["build_ext"] = CMakeBuild
-cmd_classes["clean"] = CleanCommand
 
 setup(
     version=versioneer.get_version(),
@@ -173,7 +48,6 @@ setup(
             "numpy",
             "matplotlib",
             # Build
-            "cmake",
             "pybind11",
             # Test
             "pytest",
@@ -190,6 +64,14 @@ setup(
             "htmlmin",
         ]
     },
-    ext_modules=[CMakeExtension("__psvWave_cpp", ".")],
+    ext_modules=[
+        Extension(
+            name="__psvWave_cpp",
+            sources=[
+                "src/psvWave.cpp",
+            ],
+            include_dirs=["build"],
+        )
+    ],
     zip_safe=False,
 )
